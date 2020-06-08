178f500a7bc12128692cf4fc60cd9f8e0fbef19b
AlonGvili <asingvili@gmail.com>
2020-06-08 03:53:46 +0300

    .

--- src/invoke.build.ps1
+++ src/invoke.build.ps1
@@ -8,11 +8,11 @@ task Clean {
 task Stage {
     $OutputPath = "$PSScriptRoot\output\UniversalDashboard.Antd"
     New-Item $OutputPath -Type Directory -Force
+    npm install -g webpack
+    npm install -g webpack-cli
 }
 
 task BuildJS {
-    npm install --save-dev webpack-cli
-    npm install --save-dev webpack
     npm run build
 }
 
